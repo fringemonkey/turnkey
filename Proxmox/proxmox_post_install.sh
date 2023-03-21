ec7e8c866f396808cd1f45caa53bcce989207749
fringemonkey <wesrox@gmail.com>
2023-03-20 17:41:45 -0700

    added error trapping

--- Proxmox/proxmox_post_install.sh
+++ Proxmox/proxmox_post_install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Run with 
-# wget https://raw.githubusercontent.com/fringemonkey/turnkey/3751920478010397078b1760d677c9a6b98ab313/Proxmox/proxmox_post_install.sh && chmod +x proxmox_post_install.sh && ./proxmox_post_install.sh
-#
+# Enable error trapping
+set -e
+trap 'echo -e "\033[31mError: Command failed at line $LINENO\033[0m"' ERR
 
+# Define a variable to store the current step
+current_step_file=".current_step"
+
+# Load the current step from the file, or start from the first step
+if [ -f "$current_step_file" ]; then
+    current_step=$(cat "$current_step_file")
+else
+    current_step=1
+fi
+
+# Define the progress bar function
 function progress_bar() {
     local total=$1
     local current=$2
@@ -19,62 +30,61 @@ function progress_bar() {
     echo -ne "] $percent% "
 }
 
+# Define the total number of steps
 TOTAL_STEPS=8
-current_step=1
 
-echo "1. Updating and upgrading packages..."
-apt-get update > /dev/null 2>&1 && apt-get -y upgrade > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+# Execute each step based on the current step value
+while [ $current_step -le $TOTAL_STEPS ]; do
+    case $current_step in
+        1)
+            echo "1. Updating and upgrading packages..."
+            apt-get update > /dev/null 2>&1 && apt-get -y upgrade > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "2. Securing the server..."
-echo "   - Disabling root login via SSH..."
-sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config > /dev/null 2>&1
-systemctl restart ssh > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        2)
+            echo "2. Securing the server..."
+            echo "   - Disabling root login via SSH..."
+            sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config > /dev/null 2>&1
+            systemctl restart ssh > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "3. Installing Fail2Ban..."
-apt-get -y install fail2ban > /dev/null 2>&1
-systemctl enable fail2ban > /dev/null 2>&1
-systemctl start fail2ban > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        3)
+            echo "3. Installing Fail2Ban..."
+            apt-get -y install fail2ban > /dev/null 2>&1
+            systemctl enable fail2ban > /dev/null 2>&1
+            systemctl start fail2ban > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "4. Performing post-install steps..."
-echo "   - Adding contrib and non-free repositories..."
-sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list > /dev/null 2>&1
-apt-get update > /dev/null 2>&1 && apt-get -y install vim htop tmux > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        4)
+            echo "4. Performing post-install steps..."
+            echo "   - Adding contrib and non-free repositories..."
+            sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list > /dev/null 2>&1
+            apt-get update > /dev/null 2>&1 && apt-get -y install vim htop tmux > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "5. Configuring dark mode..."
-echo "   - Cloning the Discord-like theme repository..."
-apt-get -y install git > /dev/null 2>&1
-git clone https://github.com/Weilbyte/PVEDiscordDark.git /tmp/PVEDiscordDark > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        5)
+            echo "5. Configuring dark mode..."
+            echo "   - Cloning the PVEDiscordDark repository..."
+            apt-get -y install git > /dev/null 2>&1
+            git clone https://github.com/Weilbyte/PVEDiscordDark.git /tmp/PVEDiscordDark > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "6. Running the install script..."
-bash /tmp/PVEDiscordDark/install.sh > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        6)
+            echo "6. Running the install script..."
+            cd /tmp/PVEDiscordDark
+            ./install.sh > /dev/null 2>&1
+            cd ~
+            ;;
 
-current_step=$((current_step + 1))
-echo "7. Removing temporary repository folder..."
-rm -rf /tmp/PVEDiscordDark > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        7)
+            echo "7. Removing temporary repository folder..."
+            rm -rf /tmp/PVEDiscordDark > /dev/null 2>&1
+            ;;
 
-current_step=$((current_step + 1))
-echo "8. Restarting Proxmox services..."
-systemctl restart pveproxy pvestatd > /dev/null 2>&1
-progress_bar $TOTAL_STEPS $current_step
-echo -e "\n"
+        8)
+            echo "8. Restarting Proxmox services..."
+            systemctl restart pveproxy pvestatd > /dev/null 2>&1
+            ;;
+    esac
 
-echo "Post-install script complete! The server is now secured, post-install steps are applied, and dark mode is enabled."
+    #
